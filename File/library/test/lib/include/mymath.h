6a5cff9277e829236ebfbc6438d4275642613512
Raykevin-live <raykevin121@gmail.com>
2025-04-23 23:54:09 +0800

    静态库

---
+++ File/library/test/lib/include/mymath.h
@@ -0,0 +1,8 @@
+#pragma once
+
+extern int myerrno;
+
+int add(int x, int y);
+int sub(int x, int y);
+int mul(int x, int y);
+int div(int x, int y);
---
+++ File/library/test/main.c
@@ -0,0 +1,8 @@
+#include "mymath.h"
+#include <stdio.h>
+
+int main()
+{
+	printf("1+1=%d\n", add(1, 1));
+	return 0;
+}
