97c9364f6a8c3395d494509ce44ac9848fdffd6f
Raykevin-live <raykevin121@gmail.com>
2025-05-09 14:10:40 +0800

    信号

---
+++ File/Pip/Signal/mysignal.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <unistd.h>
+#include <signal.h>
+
+using namespace std;
+
+// int: 收到了那一个信号
+void myhander(int signo){
+    cout<<"process get a signal: "<<signo<<endl;
+}
+
+int main(){
+    signal(SIGINT, myhander); // 只需设置一次，往后都有效
+    while(true){
+        cout<<"I am a man\n";
+        sleep(1);
+    }
+
+    return 0;
+}
